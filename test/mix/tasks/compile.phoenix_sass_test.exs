683e46402b4a8264a646f9d91bbe1c17bf397142
Phillip Oldham <phillip.oldham@gmail.com>
2021-01-28 12:19:47 +0100

    Use :code.priv_dir/1, add new mix task, bump deps

--- test/mix/tasks/compile.phoenix_sass_test.exs
+++ test/mix/tasks/compile.phoenix_sass_test.exs
@@ -12,56 +12,74 @@ defmodule Mix.Tasks.Compile.PhoenixSassTest do
   end
 
   setup do
-    # Temp.track!
+    # create a temporary directory to sandbox the changes,
+    # and enable tracking to delete when done
+    Temp.track!()
     {:ok, test_dir} = Temp.mkdir "_build"
 
-    System.put_env("MIX_BUILD_PATH", test_dir)
-
+    # add the project to the env
     Mix.Project.push(
       TestProject
     )
 
-    File.mkdir_p!(app_dir())
-    File.cp_r!(@fixtures_path, app_dir())
+    # set the build path to the new temp dir
+    System.put_env("MIX_BUILD_PATH", test_dir)
 
+    # "mock" the env so funcs in erlang's :code module such as
+    # :code.lib_dir/0, :code.priv_dir/1, etc. can resolve the
+    # temp files
+    ebin_path = Path.join(test_app_dir(), "ebin")
+    File.mkdir_p!(ebin_path)
+    Code.prepend_path(ebin_path)
+
+    # copy the Sass fixtures to the test priv dir
+    File.mkdir_p!(test_priv_dir())
+    File.cp_r!(@fixtures_path, test_app_dir())
+
+    # rollback on exit
     on_exit(&Mix.Project.pop/0)
 
     :ok
   end
 
   test "setup completed successfully" do
-    assert File.exists?(Path.join([app_dir(), "config", "config.exs"]))
+    assert File.exists?(Path.join([test_app_dir(), "config", "config.exs"]))
   end
 
   describe "PhoenixSass" do
     test "compiles sass to css successfully" do
-      Mix.Tasks.Compile.PhoenixSass.run([])
+      Mix.Tasks.Phx.Sass.run([])
 
-      assert File.exists?(Path.join([app_dir(), "priv", "static", "css", "main.css"]))
+      assert File.exists?(Path.join([test_priv_dir(), "static", "css", "main.css"]))
     end
 
     test "compiles sass in subdirs to css successfully" do
-      Mix.Tasks.Compile.PhoenixSass.run([])
+      Mix.Tasks.Phx.Sass.run([])
 
-      assert File.exists?(Path.join([app_dir(), "priv", "static", "css", "sub", "extra.css"]))
+      assert File.exists?(Path.join([test_priv_dir(), "static", "css", "sub", "extra.css"]))
     end
 
     test "skips sass with underscore prefix successfully" do
-      Mix.Tasks.Compile.PhoenixSass.run([])
+      Mix.Tasks.Phx.Sass.run([])
 
-      assert File.exists?(Path.join([app_dir(), "priv", "sass", "_import.sass"]))
-      assert !File.exists?(Path.join([app_dir(), "priv", "static", "css", "_import.css"]))
+      assert File.exists?(Path.join([test_priv_dir(), "sass", "_import.sass"]))
+      assert !File.exists?(Path.join([test_priv_dir(), "static", "css", "_import.css"]))
 
       assert 2 == (
-        Path.join([app_dir(), "priv", "static", "css", "**", "*.css"])
+        Path.join([test_priv_dir(), "static", "css", "**", "*.css"])
         |> Path.wildcard()
         |> Enum.count()
       )
     end
   end
 
-  defp app_dir() do
+  defp test_app_dir() do
     Mix.Project.config()
     |> Mix.Project.app_path()
+  end
+
+  defp test_priv_dir() do
+    test_app_dir()
+    |> Path.join("priv")
   end
 end
